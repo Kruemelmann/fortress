4dd1033c3086fb1da4263c9db726883b3732feee
kruemelmann <kruemelmannlp@gmail.com>
2024-12-12 19:54:50 +0100

    feat(core): working on integration into other services

---
+++ examples/gorilla_mux_inmemory/go.mod
@@ -0,0 +1,5 @@
+module github.com/kruemelmann/fortress/examples/gorilla_mux_inmemory
+
+go 1.22.2
+
+require github.com/gorilla/mux v1.8.1
---
+++ examples/gorilla_mux_inmemory/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/kruemelmann/fortress"
+)
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, `{"alive": true}`)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/health", HealthCheckHandler)
+	r.Use(fortress.AuthMiddleware)
+
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
